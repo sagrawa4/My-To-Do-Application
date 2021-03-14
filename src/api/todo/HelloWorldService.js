c55465adf171623dec193b695de76d897fe06659
Shruti Agrawal <shruti98932@gmail.com>
2021-03-14 15:04:35 -0400

    Connected React with RestAPI for Object JSon response2

--- src/api/todo/HelloWorldService.js
+++ src/api/todo/HelloWorldService.js
@@ -5,7 +5,7 @@ class HelloWorldService{
         return axios.get('http://localhost:8080/hello-world');
         //console.log('execute service')
     }
-    ///hello-world-bean
+    ///hello-world-bean for object we use : response.data.message
     executeHelloWorldBeanService(){
         return axios.get('http://localhost:8080/hello-world-bean');
         //console.log('execute service')
